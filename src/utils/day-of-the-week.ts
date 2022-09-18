e2f97ce2e4097209f9393751ea42f121867c5763
Richard Ben <frichardben@yahoo.com.br>
2022-09-18 16:42:24 -0300

    chore: added file utils

---
+++ src/utils/day-of-the-week.ts
@@ -0,0 +1,37 @@
+export const daysOfTheWeek = [ 
+    {
+        id: '0',
+        dayWeek: 'Domingo',
+        firstLetter: 'D'
+    },
+    {
+        id: '1',
+        dayWeek: 'Segunda',
+        firstLetter: 'S'
+    },
+    {
+        id: '2',
+        dayWeek: 'Terça',
+        firstLetter: 'T'
+    },
+    {
+        id: '3',
+        dayWeek: 'Quarta',
+        firstLetter: 'Q'
+    },
+    {
+        id: '4',
+        dayWeek: 'Quinta',
+        firstLetter: 'Q'
+    },
+    {
+        id: '5',
+        dayWeek: 'Sexta',
+        firstLetter: 'S'
+    },
+    {
+        id: '6',
+        dayWeek: 'Sábado',
+        firstLetter: 'S'
+    }
+    ];
